e1da8bbc463c1950427e2d874feebd222a5ee879
atulsharma111 <atulsharma61407@gmail.com>
2026-03-09 11:25:05 +0530

    mini

---
+++ practice-project2/src/app/api/auth/register/route.ts
@@ -0,0 +1,58 @@
+import connectDb from "@/lib/db";
+import User from "@/model/user.model";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+
+//status code-
+// 200-300   --- success
+// create= 201
+
+//frontend error == 400-499
+//backend error == 500
+
+export async function POST(request:NextRequest) {
+    try {
+        const {name,email,password}=await request.json()
+        await connectDb()
+        const existUser=await User.findOne({email})
+        if(existUser){
+            return NextResponse.json(
+                {message:"user already exist!"},
+                {status:400}
+            )
+        }
+
+        if(password.length<6){
+              return NextResponse.json(
+                {message:"password must be at least 6 characters!"},
+                {status:400}
+            )
+        }
+
+       const hashedPassword= await bcrypt.hash(password,10)
+       const user=await User.create({
+        name,email,password:hashedPassword
+       })
+
+           return NextResponse.json(
+                user,
+                {status:201}
+            )
+
+    } catch (error) {
+            return NextResponse.json(
+                {message:`register error ${error}`},
+                {status:500}
+            )
+    }
+}
+
+
+// // signup
+//      |
+//   check exist user
+//     |
+//   password check for 6 characters(optional)
+//   hash password using bcryptjs
+// user create
