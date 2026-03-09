535b42c87c52098520510fd9de9e95198eaf11c1
atulsharma111 <atulsharma61407@gmail.com>
2026-03-09 11:12:42 +0530

    mini

---
+++ practice-project1/src/app/destinations/[city]/page.jsx
@@ -0,0 +1,27 @@
+'use client'
+import { useParams } from 'next/navigation'
+import React from 'react'
+import parisImg from "@/assets/paris.jpg"
+import tokyoImg from "@/assets/tokyo.jpg"
+import nyImg from "@/assets/new york.webp"
+import Image from 'next/image'
+function page({params}) {
+    const {city}=useParams()
+  return (
+    <div className='text-white mt-[100px] w-[50%] '>
+      {city} is the beautiful city
+      {
+        city=="Paris" &&  <Image src={parisImg} width={400} height={400} alt='paris img'/>
+      }
+       {
+        city=="Tokyo" &&  <Image src={tokyoImg} width={400} height={400} alt='tokyo img'/>
+      }
+       {
+        city=="NewYork" &&  <Image src={nyImg} width={400} height={400} alt='ny img'/>
+      }
+     
+    </div>
+  )
+}
+
+export default page
---
+++ practice-project1/src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='flex justify-center items-center text-white font-bold text-[40px] h-full'>
+      Welcome to our Travel Guide Website
+    </div>
+  )
+}
+
+export default page
